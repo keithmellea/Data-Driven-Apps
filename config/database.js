09e448ed2b80b4ef758f16d416a1259bb55d08a9
Keith Mellea <keithmellea@gmail.com>
2021-03-25 13:11:35 -0400

    5th

---
+++ config/database.js
@@ -0,0 +1,40 @@
+{
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
+
+const {
+  username,
+  password,
+  database,
+  host,
+} = require('./index').DB;
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
+  },
+};
